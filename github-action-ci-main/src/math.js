9b0cf37ecc54998ebd406aa8678119ae57813669
alaanajeeb10 <131193760+alaanajeeb10@users.noreply.github.com>
2025-11-28 10:29:42 +0200

    first commit

---
+++ github-action-ci-main/src/math.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function doul(a){
+    return a % 2 == 0;
+}
+npm 
+function p(a,b){
+    return a-b;
+}
+module.exports = {
+    sum,
+    doul,
+    p
+}
